2470450cba24899da2e51472768abd58b200f6b7
TheFigmo <figmo@figmo.com>
2017-01-24 11:19:46 -0700

    Add files via upload

---
+++ Models/ConfigurationModel.cs
@@ -0,0 +1,20 @@
+﻿using Nop.Web.Framework;
+using Nop.Web.Framework.Mvc;
+
+namespace RG.Plugin.eBayCommander.Models
+{
+    public class ConfigurationModel : BaseNopModel
+    {
+        public int ActiveStoreScopeConfiguration { get; set; }
+
+        [NopResourceDisplayName("Plugins.eBayCommander.eBayToken")]
+        public string eBayToken { get; set; }
+
+        [NopResourceDisplayName("Plugins.eBayCommander.eBayDefaultStoreId")]
+        public int eBayDefaultStoreId { get; set; }
+
+        [NopResourceDisplayName("Plugins.eBayCommander.eBayDefaultProductId")]
+        public int eBayDefaultProductId { get; set; }
+
+    }
+}
